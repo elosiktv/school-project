9b6b7c3dcf817dabadf69996b2f78b72089fb91b
Daniel Dąbrowski <elosik123@onet.pl>
2018-10-01 15:28:42 +0200

    Sass, one slide

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 
-gulp.task('serve', () => {
+gulp.task('serve', ['sass'], () => {
     browserSync.init({
         server: './'
     });
+    gulp.watch('./scss/*.scss', ['sass']);
     gulp.watch('./*').on('change', browserSync.reload);
+});
+
+gulp.task('sass', () => {
+    return gulp.src('./scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve']);
